e3296f2bfab912563a9202012ddb573bb35d5852
junyoung-hong <junyoung.hong@stu.ocsb.ca>
2026-04-10 12:53:28 -0400

    CODE

--- main.ts
+++ main.ts
@@ -1,8 +1,93 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2026 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Junyoung
+ * Created on: Apr 2026
+ * This program shows while loops in action
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let loopCounterX: number = 0
+let loopCounterY: number = 0
+
+// show happy face
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+    loopCounterX = 0
+    loopCounterY = 0
+
+    while (loopCounterX < 5) {
+        sprite.set(LedSpriteProperty.X, loopCounterX)
+        loopCounterX++
+        basic.pause(500)
+    }
+
+    while (loopCounterY < 5) {
+        sprite.set(LedSpriteProperty.Y, loopCounterY)
+        loopCounterY++
+        basic.pause(500)
+    }
+
+    if (loopCounterX == 5) {
+        while (loopCounterX >= 0) {
+            sprite.set(LedSpriteProperty.X, loopCounterX)
+            loopCounterX--
+            basic.pause(500)
+        }
+    }
+
+    if (loopCounterY == 5) {
+        while (loopCounterY >= 0) {
+            sprite.set(LedSpriteProperty.Y, loopCounterY)
+            loopCounterY--
+            basic.pause(500)
+        }
+    }
+
+    sprite.delete()
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+    loopCounterX = 0
+    loopCounterY = 0
+
+    while (loopCounterY < 5) {
+        sprite.set(LedSpriteProperty.Y, loopCounterY)
+        loopCounterY++
+        basic.pause(500)
+    }
+
+    while (loopCounterX < 5) {
+        sprite.set(LedSpriteProperty.X, loopCounterX)
+        loopCounterX++
+        basic.pause(500)
+    }
+
+    if (loopCounterY == 5) {
+        while (loopCounterY >= 0) {
+            sprite.set(LedSpriteProperty.Y, loopCounterY)
+            loopCounterY--
+            basic.pause(500)
+        }
+    }
+
+    if (loopCounterX == 5) {
+        while (loopCounterX >= 0) {
+            sprite.set(LedSpriteProperty.X, loopCounterX)
+            loopCounterX--
+            basic.pause(500)
+        }
+    }
+
+    sprite.delete()
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
